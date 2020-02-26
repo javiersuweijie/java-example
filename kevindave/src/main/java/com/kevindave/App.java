d9e2be322f7df56f15aac3c2f898abd4c381abc6
Javier Su <javier.su.weijie@gmail.com>
2020-02-26 07:08:44 +0700

    fix(import) Fixed xendit import

--- kevindave/src/main/java/com/kevindave/App.java
+++ kevindave/src/main/java/com/kevindave/App.java
@@ -1,5 +1,7 @@
 package com.kevindave;
 
+import com.xendit.Xendit;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        Xendit.getApiKey();
         System.out.println( "Hello World!" );
     }
 }
